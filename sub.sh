c4b655a7743237a40bcb215846b700c48b063d0e
Cihan Mehmet DOĞAN (CMD) <cihanmehmet@users.noreply.github.com>
2020-01-31 23:51:22 +0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -6,7 +6,7 @@ echo "[g] Github => https://github.com/cihanmehmet"
 echo "[#] curl -sL https://raw.githubusercontent.com/cihanmehmet/sub.sh/master/sub.sh | bash -s bing.com"
 echo "[#] curl -sL https://git.io/JesKK | bash -s tesla.com"
 echo "███████████████████████████████████████████████████████████████████████████████████████████████"
-
+# sub.sh version 1.0.4
 if [[ $# -eq 0 ]] ;
 then
 	echo "Usage: bash sub.sh bing.com"
@@ -47,14 +47,18 @@ else
 	    echo "[+] Suip.biz Findomain Over"
 
 	echo "——————————————————————————————————$1 SUBDOMAIN————————————————————————————————————————————"
-	cat $1.txt|sort -u|egrep -v "//|:|,"|tee $1.txt
+	#cat $1.txt|sort -u|egrep -v "//|:|,"|tee $1.txt
+	cat $1.txt|sort -u|egrep -v "//|:|,| |_|@" |grep -o "\w.*$1"|tee $1.txt
 	echo "- - - - - - - - - - - - - - - - - $1 ALIVE SUBDOMAIN - - - - - - - - - - - - - - - - - - -"
-    cat $1.txt|httprobe -t 15000 -c 50|cut -d "/" -f3|sort -u |tee alive_$1.txt 
+        cat $1.txt|httprobe -t 15000 -c 50|cut -d "/" -f3|sort -u |tee alive_$1.txt 
 	echo "███████████████████████████████████████████████████████████████████████████████████████████"
 	echo "Detect Subdomain $(wc -l $1.txt|awk '{ print $1 }' )" "=> ${1}"
 	echo "File Location : "$(pwd)/"$1.txt"
 	echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
 	echo "Detect Alive Subdomain $(wc -l alive_$1.txt|awk '{ print $1 }' )" "=> ${1}"
 	echo "File Location : "$(pwd)/"alive_$1.txt"
+	
+	#cat $1.txt|python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $1.json
+        #cat alive_$1.txt|python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" >live_$1.json
 
 fi
