158e454d0ab836b6c79b39b4c60e3300ede7a6c1
Cihan Mehmet DOĞAN (CMD) <cihanmehmet@users.noreply.github.com>
2020-04-19 18:48:01 +0300

    Delete nmap_sn.sh

--- nmap_sn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nmap -sn -iL $1 |grep "Nmap scan report for"|grep -Eo "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"|sort -u |tee $1.txt
-
-echo "Detect IP $(wc -l $1.txt|awk '{ print $1 }' )" "=> result_${1}" "saved"
-echo "File Location : "$(pwd)/"result_$1"
